d57739d99a0a5ec8c53a97024d2ada9f9739e92a
Cody Wittmaack <cwittmaack@hotmail.com>
2022-10-19 11:37:18 -0500

    finalfixes

--- Server/controller.js
+++ Server/controller.js
@@ -2,13 +2,13 @@ const path = require('path')
 
 module.exports = {
     getHTML: (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'))
+        res.sendFile(path.join(__dirname, '../Public/index.html'))
     },
     getCSS: (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/styles.css'))
+        res.sendFile(path.join(__dirname, '../Public/styles.css'))
     },
     getJS: (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/main.js'))
+        res.sendFile(path.join(__dirname, '../Public/main.js'))
     }
 }
 
--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static ('public'))
+app.use(express.static ('Public'))
 
 const {getHTML, getCSS, getJS} = require('./controller')
 
